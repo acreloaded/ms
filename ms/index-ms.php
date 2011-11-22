04beb28291fcc0a115ecb3c3fccf10a288e4886e
theonlypwner <theonlypwner@users.noreply.github.com>
2011-11-22 00:11:04 +0000

    Import old v1.0 content

---
+++ ms/index-ms.php
@@ -0,0 +1,6 @@
+<h1>AssaultCube Reloaded Master Server</h1>
+<p>Use <a href="/cube" >/cube</a> for CubeScript server list</p>
+<p>Use <a href="/json" >/json</a> for JSON server list</p>
+<p>Use <a href="/bans" >/bans</a> for a verbose ban list</p>
+<p>Your server may register any port between <?=$config['servers']['minport']?> and <?=$config['servers']['maxport']?> if:</p>
+<p>your server is running protocol <?=$config['servers']['minprotocol']?> or later.</p>
