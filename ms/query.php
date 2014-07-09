6729889bdc929407872566ac52564e7c965ab3e2
Victor <theonlypwner@users.noreply.github.com>
2014-07-09 13:10:03 -0600

    Add JSON server list

--- ms/query.php
+++ ms/query.php
@@ -8,8 +8,8 @@ if ( isset( $_GET['c'] ) ) echo $_GET['c'].'(';
 if ( !isset( $_GET['q'] ) )
 	$_GET['q'] = '';
 switch ( $_GET['q'] ) {
-	// number of servers
 case 'servers':
+	// number of servers
 	$sockcap = $settings['check-socket'] ? $settings['check-socket'] : 255;
 	$q = $db->fetch_array( $db->simple_select(
 			"acrms_servers", "COUNT(*) As n", "failures < $sockcap AND proto >= {$settings['minprotocol']}"
@@ -17,6 +17,23 @@ case 'servers':
 	// write output
 	$json = array( 'active' => $q['n'], 'total' => $cache->read( "acrms_servs" ) );
 	$json['hidden'] = $json['total'] - $json['active'];
+	break;
+
+case 'json':
+	// server list
+	$sockcap = $settings['check-socket'] ? $settings['check-socket'] : 255;
+	$q = $db->simple_select( "acrms_servers", "ip,port", "failures < $sockcap AND proto >= {$settings['minprotocol']}" );
+	$servers = array();
+	while ( $r = $db->fetch_array( $q ) ) {
+		$host = $r['ip'];
+		$port = $r['port'];
+		// hostname subsitution
+		if ( isset( $settings['translations'][$host] ) )
+			$host = $settings['translations'][$host];
+		// add to list
+		$servers[] = array( $host, (int)$port );
+	}
+	$json = array( 'servers'=>$servers );
 	break;
 	// unknown
 default:
